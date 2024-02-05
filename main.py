57a9444554a63ad7b3ce6bb1da92b0b6efa274dd
AdityaSurve <surveaditya521@gmail.com>
2024-02-05 22:54:01 +0530

    data ingestion pipeline

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from heartDiseaseClassification import logger
+from heartDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is a test log message.")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<")
+    data_ingesetion = DataIngestionTrainingPipeline()
+    data_ingesetion.main()
+    logger.info(
+        f">>>>> Stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
